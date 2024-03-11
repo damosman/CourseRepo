53ac926038a32d57b35cf058c5358a5f24bd157e
da <da@seneco.dk>
2024-03-11 02:27:04 +0100

    Added Tests branch and dependencies

---
+++ Courses.UnitTests/Services/CourseCreateReqTest.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace Courses.UnitTests.Services
+{
+    public class CourseCreateReqTest
+    {
+        [Fact]
+        public void CreateReq()
+        {
+            //Arrange
+            CourseCreateReqTest courseCreateReqTest = new CourseCreateReqTest();    
+
+            //Act
+
+            //Assert
+        }
+    }
+}
